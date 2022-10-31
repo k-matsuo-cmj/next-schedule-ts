8b6ea9e0203fc87273397c3bc77a12750a763ed0
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-10-31 17:05:49 +0900

    header layout

--- components/header.tsx
+++ components/header.tsx
@@ -1,2 +1,54 @@
-const Header = () => <><h1>Next Schedule App</h1></>;
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { NextPage } from "next";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import jwt from "jsonwebtoken";
+import { DecodedType } from "../utils/types";
+import { secretKey } from "../utils/secretKey";
+
+const Header: NextPage = () => {
+  const [loginUser, setLoginUser] = useState<DecodedType | null>(null);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decoded = jwt.verify(token, secretKey) as DecodedType;
+        setLoginUser({ ...decoded });
+      } catch (err) {}
+    }
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setLoginUser(null);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar position="static" component="nav">
+        <Toolbar>
+          <Link href="/">
+            <Typography variant="h6">Next Schedule App</Typography>
+          </Link>
+          <Box>
+            <Link href="/schedule/create">
+              <Button sx={{ color: "#fff", mx: 2 }}>TEST</Button>
+            </Link>
+          </Box>
+          {!loginUser && (
+            <Link href="/user/login">
+              <Typography>LOGIN</Typography>
+            </Link>
+          )}
+          {loginUser && <Typography>HELLO {loginUser.userName}</Typography>}
+          {loginUser && (
+            <Button sx={{ color: "#fff", mx: 2 }} onClick={logout}>
+              LOGOUT
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 export default Header;
--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -1,36 +1,50 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 const LoginPage: NextPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+  const router = useRouter();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       const response = await fetch("http://localhost:3000/api/user/login", {
         method: "POST",
         headers: {
-          "Accept": "application/json",
+          Accept: "application/json",
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
           email: email,
           password: password,
-        })});
+        }),
+      });
       const jsonData = await response.json();
       localStorage.setItem("token", jsonData.token);
       alert("Login Success");
-    } catch(err) {
+      router.push("/");
+    } catch (err) {
       alert("Login Fail");
     }
   };
-  return (<>
-  <h1>ログインページ</h1>
-  <form onSubmit={handleSubmit}>
-    <input value={email} onChange={(e)=>setEmail(e.target.value)} name="email" />
-    <input value={password} onChange={(e)=>setPassword(e.target.value)} name="password" />
-    <button type="submit">ログイン</button>
-  </form>
-  </>);
+  return (
+    <>
+      <h1>ログインページ</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+        />
+        <button type="submit">ログイン</button>
+      </form>
+    </>
+  );
 };
 export default LoginPage;
